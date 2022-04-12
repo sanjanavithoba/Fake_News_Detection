8a6f11dff5afde522c993b7a0c226c3f5f628888
sanjanavithoba <sanjanavithoba@hotmail.com>
2022-04-12 23:03:21 +0530

    update code

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, request, render_template
 import pickle
 
-
+vector = pickle.load(open("vectorizer.pkl",'rb'))
+model = pickle.load(open("finalized_model.pkl", 'rb'))
 
 app = Flask(__name__)
-
+  
 @app.route('/')
-def home():
-   return render_template('index.html')
-
-
+def methods():
+   return render_template ("interface.html")
+ 
 @app.route('/prediction', methods=['GET','POST'])
 def prediction():
-    if request.method == "POST":
-       news=request.form['news']
-       print(news)
-      
-      
-       predict = model.predict(vector.transform([news]))[0]
-       print(predict)
-  
-    return render_template('prediction.html', prediction_text='News Headline is -> {}'.format(predict))
+   
+   if request.method==("POST"):
+      news =str(request.form['news'])
 
+      print(news)
+      prediction = model.prediction(vector.transform([news]))[0]
+      print(prediction)
 
-def home():
-   return render_template('prediction.html')
+      return render_template(prediction_text ="News Headline is-> {}".format(prediction))
+      
+   else:
+      request.methods=("GET")
+      return render_template("prediction.html")
 
 
 
 if __name__=='__main__':
-   app.run()                
+   app.run(debug=True) 
